ad31188bdb71b0e7d639f42501bfc1a1a51f4d2c
peranut-alb <peranut@origami.life>
2025-09-03 14:39:55 +0700

    [Update] ปรับเมนูหน้าหลักเป็น Greentech

--- study/views/profile.php
+++ study/views/profile.php
@@ -1,7 +1,6 @@
 <?php
     session_start();
     require_once("../../lib/connect_sqli.php");
-    include_once("../../login_history.php");
     global $mysqli;
 
     // Check if the student_id is set in the session
