708d4fe1ffa6d03254a6d7c630c513333ef7689f
chakritgui <chakrit@trandar.com>
2025-09-16 19:13:42 +0700

    [Update] Classroom Source

--- study/actions/class.php
+++ study/actions/class.php
@@ -1,57 +1,38 @@
 <?php
-session_start();
-$base_include = $_SERVER['DOCUMENT_ROOT'];
-$base_path = '';
-if ($_SERVER['HTTP_HOST'] == 'localhost') {
-    $request_uri = $_SERVER['REQUEST_URI'];
-    $exl_path = explode('/', $request_uri);
-    if (!file_exists($base_include . "/dashboard.php")) {
-        $base_path .= "/" . $exl_path[1];
+    session_start();
+    $base_include = $_SERVER['DOCUMENT_ROOT'];
+    $base_path = '';
+    if ($_SERVER['HTTP_HOST'] == 'localhost') {
+        $request_uri = $_SERVER['REQUEST_URI'];
+        $exl_path = explode('/', $request_uri);
+        if (!file_exists($base_include . "/dashboard.php")) {
+            $base_path .= "/" . $exl_path[1];
+        }
+        $base_include .= "/" . $exl_path[1];
     }
-    $base_include .= "/" . $exl_path[1];
-}
-define('BASE_PATH', $base_path);
-define('BASE_INCLUDE', $base_include);
-require_once $base_include . '/lib/connect_sqli.php';
-
-$student_id = $_SESSION['student_id'] ? $_SESSION['student_id'] : null;
-$student_classroom_id = select_data("classroom_id", "classroom_student_join", "WHERE student_id = '{$student_id}'");
-
-$classroom_id = $student_classroom_id[0]['classroom_id'];
-
-
-if ($_GET['action'] === 'loadAlumni') {
-
-    $student_alumni = select_data(
-        "classroom_id, classroom_name, classroom_information", 
-        "classroom_template", 
-        "WHERE classroom_id = '{$classroom_id}' AND
-        comp_id = '{$_SESSION['comp_id']}'");
-
-    echo json_encode($student_alumni);
-}
-
-
-if ($_POST['action'] === 'loadClass' && !empty($_POST['classroom_id'])) {
-
-    $class_id = $_POST['classroom_id'];
-    
-    $course_data = select_data(
-    "cc.course_type,
-    c.trn_id AS course_id,
-    c.trn_subject AS course_name,
-    c.picture_title AS course_cover",
-    "classroom_course AS cc JOIN ot_training_list AS c on cc.course_ref_id = c.trn_id",
-    "WHERE cc.comp_id = '{$_SESSION['comp_id']}' 
-        AND cc.classroom_id = '{$class_id}' 
-        AND cc.status = 0"
-    );
-
-    echo json_encode($course_data);
-
-    //  $response = [
-    //     'class_id' => $class_id,
-    //     'course_data' => $course_data
-    // ];
-    // echo json_encode($response);
-}
+    define('BASE_PATH', $base_path);
+    define('BASE_INCLUDE', $base_include);
+    require_once $base_include . '/lib/connect_sqli.php';
+    $student_id = $_SESSION['student_id'] ? $_SESSION['student_id'] : null;
+    $student_classroom_id = select_data("classroom_id", "classroom_student_join", "WHERE student_id = '{$student_id}'");
+    $classroom_id = $student_classroom_id[0]['classroom_id'];
+    if ($_GET['action'] === 'loadAlumni') {
+        $student_alumni = select_data(
+            "classroom_id, classroom_name, classroom_information", 
+            "classroom_template", 
+            "WHERE classroom_id = '{$classroom_id}'");
+        echo json_encode($student_alumni);
+    }
+    if ($_POST['action'] === 'loadClass' && !empty($_POST['classroom_id'])) {
+        $class_id = $_POST['classroom_id'];
+        $course_data = select_data(
+        "cc.course_type,
+        c.trn_id AS course_id,
+        c.trn_subject AS course_name,
+        c.picture_title AS course_cover",
+        "classroom_course AS cc JOIN ot_training_list AS c on cc.course_ref_id = c.trn_id",
+        "WHERE cc.classroom_id = '{$class_id}' 
+            AND cc.status = 0"
+        );
+        echo json_encode($course_data);
+    }
---
+++ study/redirect.php
@@ -0,0 +1,115 @@
+<?php
+    session_start();
+    $base_include = $_SERVER['DOCUMENT_ROOT'];
+    $base_path = '';
+    if($_SERVER['HTTP_HOST'] == 'localhost'){
+        $request_uri = $_SERVER['REQUEST_URI'];
+        $exl_path = explode('/',$request_uri);
+        if(!file_exists($base_include."/dashboard.php")){
+            $base_path .= "/".$exl_path[1];
+        }
+        $base_include .= "/".$exl_path[1];
+    }
+    define('BASE_PATH', $base_path);
+    define('BASE_INCLUDE', $base_include);
+    require_once $base_include.'/lib/connect_sqli.php';
+    $student_id = (isset($_SESSION['student_id'])) ? $_SESSION['student_id'] : '';
+    $id = (isset($_GET['id'])) ? $_GET['id'] : '';
+    $cid = (isset($_GET['cid'])) ? base64_decode($_GET['cid']) : '';
+    if(!$_SESSION && empty($student_id) && empty($id) && empty($cid)) {
+        echo '<script language="javascript">window.location="/";</script>';
+        exit;
+    }
+    $val = explode('_', $id);
+    if (count($val) == 3) {
+        $learning_id = $val[2];
+        $learning_type = 'learning_map';
+    } else {
+        $learning_id = $val[1];
+        $learning_type = 'course';
+    }
+    $employee = select_data(
+        "emp_id, classroom_id, comp_id", "classroom_student_join", "where student_id = '{$student_id}' and classroom_id = '{$cid}'"
+    );
+    $emp_id = $employee[0]['emp_id'];
+    $classroom_id = $employee[0]['classroom_id'];
+    $comp_id = $employee[0]['comp_id'];
+    if(empty($emp_id)) {
+        $employees = select_data(
+            "emp_id", "classroom_student_join", "where student_id = '{$student_id}' and comp_id = '{$comp_id}'"
+        );
+        $emp_id = $employees[0]['emp_id'];
+        if(empty($emp_id)) {
+            $students = select_data(
+                "student_firstname_en, student_lastname_en, student_firstname_th, student_lastname_th, student_email", "classroom_student", "where student_id = '{$student_id}'"
+            ); 
+            $firstname_en = escape_string($students[0]['student_firstname_en']);
+            $lastname_en = escape_string($students[0]['student_lastname_en']);
+            $firstname_th = escape_string($students[0]['student_firstname_th']);
+            $lastname_th = escape_string($students[0]['student_lastname_th']);
+            $email = escape_string($students[0]['student_email']);
+            $emp_id = insert_data(
+                "m_employee", "(email, comp_id, emp_type)", "('{$email}', '{$comp_id}', 'student')"
+            );
+            if($emp_id) {
+                insert_data(
+                    "m_employee_info", "(emp_id, firstname, lastname, firstname_th, lastname_th)", "('{$emp_id}', '{$firstname_en}', '{$lastname_en}', '{$firstname_th}', '{$lastname_th}')"
+                );
+                update_data(
+                    "classroom_student_join", "emp_id = '{$emp_id}'", "student_id = '{$student_id}' and classroom_id = '{$classroom_id}'"
+                );
+            }
+        }
+    }
+    $classrooms = select_data(
+        "date_format(classroom_start, '%Y-%m-%d') as classroom_start, date_format(classroom_end, '%Y-%m-%d') as classroom_end", "classroom_template", "where classroom_id = '{$classroom_id}'"
+    );
+    $classroom_start = $classrooms[0]['classroom_start'];
+    $classroom_end = $classrooms[0]['classroom_end'];
+    $courses = select_data(
+        "course_ref_id, course_type", 
+        "classroom_course",
+        "where classroom_id = '{$classroom_id}' and status = 0"
+    );
+    foreach($courses as $course) {
+        if($course['course_type'] == 'course') {
+            $exist = select_data(
+                "trainee_id",
+                "ot_employee_training",
+                "where emp_id = '{$emp_id}' and trn_id = '{$course['course_ref_id']}'"
+            );
+            if(!empty($exist)) {
+                update_data(
+                    "ot_employee_training",
+                    "emp_trn_start_date_time = '{$classroom_start}', emp_trn_end_date_time = '{$classroom_end}', status = 0, emp_modify = '{$emp_id}', date_modify = NOW()",
+                    "emp_id = '{$emp_id}' and trn_id = '{$course['course_ref_id']}'"
+                );
+            } else {
+                insert_data(
+                    "ot_employee_training", "(emp_id, trainee_type, trn_id, emp_trn_start_date_time, emp_trn_end_date_time, status, emp_create, date_create, emp_modify, date_modify)", "('{$emp_id}', 'student', '{$course['course_ref_id']}', '{$classroom_start}', '{$classroom_end}', 0, '{$emp_id}', NOW(), '{$emp_id}', NOW())"
+                );
+            }
+        } else {
+            $exist = select_data(
+                "trainee_id",
+                "ot_learning_map_emp",
+                "where emp_id = '{$emp_id}' and learning_map_id = '{$course['course_ref_id']}'"
+            );
+            if(!empty($exist)) {
+                update_data(
+                    "ot_learning_map_emp",
+                    "learning_map_emp_start = '{$classroom_start}', learning_map_emp_end = '{$classroom_end}', status = 0, emp_modify = '{$emp_id}', date_modify = NOW()",
+                    "emp_id = '{$emp_id}' and learning_map_id = '{$course['course_ref_id']}'"
+                );
+            } else {
+                insert_data(
+                    "ot_learning_map_emp", "(learning_map_id, emp_id, trainee_type, learning_map_emp_start, learning_map_emp_end, comp_id, status, emp_create, date_create, emp_modify, date_modify)", "('{$course['course_ref_id']}', '{$emp_id}', 'student', '{$classroom_start}', '{$classroom_end}', '{$comp_id}', 0, '{$emp_id}', NOW(), '{$emp_id}', NOW())"
+                );
+            }
+            
+        }
+    }
+    $_SESSION['emp_id'] = $emp_id;
+    $_SESSION['comp_id'] = $comp_id;
+    header('Location: /academy/redirect.php?id='.$id);
+?>
