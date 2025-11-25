bab6aa746b67656c0c0bc93b9aee9253011f17df
Aphisit <Apisit@origami.life>
2025-11-25 14:50:57 +0700

    เพิ่มการตรวจจับใบหน้าแบบเช็คจาก classroom ก่อน

--- management/js/photo.js
+++ management/js/photo.js
@@ -1,8 +1,13 @@
+// js/photo.js ที่แก้ไขแล้ว
 function buildPhotoPage() {
+    // **✅ ดึง classroom_id จาก Hidden Field ในหน้าแม่**
+    var classroomId = $('#classroom_id').val(); 
     var contentUrl = '/classroom/management/actions/photo.php';
+    
     $.ajax({
         url: contentUrl,
-        type: 'GET',
+        type: 'POST', // **เปลี่ยนเป็น POST เพื่อส่ง classroom_id ได้ง่าย**
+        data: { classroom_id: classroomId }, // **✅ ส่ง classroom_id ไปด้วย**
         dataType: 'html',
         beforeSend: function() {
             $(".content-container").html('<div class="text-center" style="padding: 50px;"><i class="fas fa-spinner fa-spin fa-2x"></i> Loading...</div>');
@@ -11,7 +16,8 @@ function buildPhotoPage() {
     .done(function(data) {
         $(".content-container").html(data);
         
-    
+        // **✅ ตั้งค่า Hidden Field ในฟอร์มย่อย (photo.php) ให้ถูกต้องอีกครั้ง**
+        $('#form_classroom_id').val(classroomId); 
     })
     .fail(function() {
         $(".content-container").html('<div class="alert alert-danger">ไม่สามารถโหลดเนื้อหา **เพิ่มรูปภาพกลุ่มสำหรับ Face Recognition** ได้</div>');
@@ -22,8 +28,5 @@ $(document).ready(function() {
     $('a.get-management[data-page="photo"]').on('click', function(e) {
         e.preventDefault(); 
         buildPhotoPage();
-        
-       
     });
 });
-
